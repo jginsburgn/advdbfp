6ac2dc9b9a220651781b96c65241d9843a2ba553
Jonathan Ginsburg <jginsburgn@gmail.com>
2016-05-11 05:57:49 -0500

    CRUD ready for Batches of Raw Products (note that when trying to erase a Raw Product with existing batches it cannot be done); also, a script for loading mock data to neo4j was created

--- database/neo4j.js
+++ database/neo4j.js
@@ -9,6 +9,9 @@ module.exports = {
     insertN: function (values, kind, answer) {
         db.insertNode(values, kind, answer);
     },
+    forceRemoval: function(id, answer){
+        db.cypherQuery("match (n) where id(n)=" + id + " detach delete n;", answer);
+    },
     removeN: function (id, answer) {
         db.deleteNode(id, answer);
         // db.deleteNode(id, function(err, node){
@@ -29,6 +32,9 @@ module.exports = {
     },
     getBRPs: function(pid, answer) {
         db.cypherQuery("match (rp:RawProduct)-[r:BatchRPR]->(brp) where id(rp)=" + pid + " return brp;", answer);
+    },
+    addBRP: function(pid, amount, expd, expm, expy, answer){
+        db.cypherQuery("match (rp) where id(rp)=" + pid + ' create (rp)-[r:BatchRPR]->(brp:BatchRawProduct {amount:"' + amount + '", expD:"' + expd + '", expM:"' + expm + '", expY:"' + expy + '"}) return brp;', answer);
     }
 };
 
--- routes/batchrawproducts/batchrawproducthandler.js
+++ routes/batchrawproducts/batchrawproducthandler.js
@@ -21,16 +21,119 @@ function runPost(action, req, res){
         case "show":
             showBatches(req, res);
             break;
+        case "add":
+            addBatch(req, res);
+            break;
+        case "commitadd":
+            commitAdd(req, res);
+            break;
+        case "edit":
+            editBatch(req, res);
+            break;
+        case "commitedit":
+            commitEdit(req, res);
+            break;
+        case "delete":
+            commitDelete(req, res);
+            break;
         default:
             console.log("Unrecognized action for batch raw products handler post:" + action);
             res.redirect(res.get('referer'));
     }
 }
 
+function commitDelete(req, res){
+    db.forceRemoval(req.body.id, function(error, result, other){
+        db.getBRPs(req.body.productid, function(err, result){
+            if(err) throw err;
+            res.render('batchrawproducts', {
+                productid:req.body.productid,
+                brps: result.data,
+                titlename:req.body.productname,
+                unit: req.body.unit
+            });
+        });
+    });
+}
+
+function editBatch(req, res){
+    res.render('batchrawproduct', {
+        productid: req.body.productid,
+        productname: req.body.productname,
+        amount: req.body.amount,
+        expd: req.body.expd,
+        expm: req.body.expm,
+        expy: req.body.expy,
+        brpid: req.body.id,
+        verb: "Edit",
+        action: "commitedit",
+        title: "Editing Batch for: " + req.body.productname,
+        unit: req.body.unit
+    });
+}
+
+function commitEdit(req, res){
+    var amount = req.body.amount;
+    var expd = req.body.expd;
+    var expm = req.body.expm;
+    var expy = req.body.expy;
+    var newNode = {amount: amount, expD: expd, expM: expm, expY: expy};
+    db.editN(
+        req.body.brpid,
+        newNode,
+        function(err, node){
+            if(err) throw err;
+            if(node === true){
+                db.getBRPs(req.body.pid, function(err, result){
+                    if(err) throw err;
+                    res.render('batchrawproducts', {
+                        productid:req.body.pid,
+                        brps: result.data,
+                        titlename:req.body.pname,
+                        unit: req.body.unit
+                    });
+                });
+            } else {
+                // node not found, hence not updated
+            }
+        }
+    );
+}
+
 function showBatches(req, res) {
     db.getBRPs(req.body.id, function(err, result){
         if(err) throw err;
-        res.render('batchrawproducts', { brps: result.data, titlename:req.body.pname });
+        res.render('batchrawproducts', {
+            productid:req.body.id,
+            brps: result.data,
+            titlename:req.body.pname,
+            unit: req.body.unit
+        });
+    });
+}
+
+function addBatch(req, res){
+    res.render('batchrawproduct', {
+        productid: req.body.productid,
+        productname: req.body.productname,
+        verb: "Add",
+        action: "commitadd",
+        title: "Adding Batch for: " + req.body.productname,
+        unit: req.body.unit
+    });
+}
+
+function commitAdd(req, res){
+    db.addBRP(req.body.pid, req.body.amount, req.body.expd, req.body.expm, req.body.expy, function(err, result) {
+        db.getBRPs(req.body.pid, function(err, result){
+            if(err) throw err;
+            res.render('batchrawproducts', {
+                productid:req.body.pid,
+                brps: result.data,
+                titlename:req.body.pname,
+                unit: req.body.unit
+            });
+        });
     });
 }
 
